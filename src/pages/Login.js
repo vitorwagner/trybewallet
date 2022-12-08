87413a55582cc2a1bb775a664a17c9a9e3780554
Vitor Wagner <vitormwagner@gmail.com>
2022-12-08 16:10:55 -0300

    Adiciona página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  // Ref para regex email: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+
+  validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
+    return emailRegex.test(email);
+  };
+
+  validatePassword = (password) => {
+    const minPasswordLength = 6;
+    return password.length < minPasswordLength;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(userLogin(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+          placeholder="e-mail"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          placeholder="senha"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ !this.validateEmail(email) || this.validatePassword(password) }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
